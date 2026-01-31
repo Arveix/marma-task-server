71910580896c03a5a47483894ee8da9822d8dd0f
Samarth Kavthale <arveix20@gmail.com>
2026-01-31 10:58:52 +0530

    debugging:

--- config/db.js
+++ config/db.js
@@ -5,8 +5,9 @@ dotenv.config();
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(`${process.env.MONGODB_URI}/marma_task_db`);
-        console.log('MongoDB conneeeeeeected');
+        // await mongoose.connect(`${process.env.MONGODB_URI}/marma_task_db`);
+        await mongoose.connect(`mongodb+srv://Vercel-Admin-marma-task-db:7DyHmeUL7kUo5aKs@marma-task-db.dwqmtll.mongodb.net/marma_task_db`);
+        console.log('MongoDB connected');
         console.log("Connected DB:", mongoose.connection.name);
     } catch (error) {
         console.error('MongoDB connection failed:', error.message);
